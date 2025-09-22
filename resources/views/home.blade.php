caf075b55c2980413923ce8addedac09b80a2998
Bogdanbana <bogdannagornyj06@gmail.com>
2025-09-22 10:28:04 +0500

    home web

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Домашняя страница</title>
+<!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+<!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <header>
+        
+    </header>
+    <body>
+        
+    </body>
+    <footer>
+
+    </footer>
+</body>
+</html>
